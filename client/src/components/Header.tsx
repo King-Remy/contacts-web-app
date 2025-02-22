412c11d84e7abaa5976ab22586a96f1f6ab8413b
King Remy Igbokwe <king.igbokwe@gmail.com>
2025-02-22 10:49:21 +0000

    added header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+import { Container } from 'react-bootstrap'
+
+export default function Header() {
+    return (
+        <header>
+            <Container className='d-flex justify-content-center'>
+                <h1>PhoneBook Application</h1>
+            </Container>
+        </header>
+    );
+};
